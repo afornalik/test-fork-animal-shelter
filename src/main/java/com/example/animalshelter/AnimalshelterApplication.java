48b113b759abbc7ff21c1495a77dc6a04d78f794
afornalik <43099315+afornalik@users.noreply.github.com>
2022-08-22 11:38:43 +0200

    Main test commit

--- src/main/java/com/example/animalshelter/AnimalshelterApplication.java
+++ src/main/java/com/example/animalshelter/AnimalshelterApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnimalshelterApplication {
 
+	int testValue = 1;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalshelterApplication.class, args);
 	}
+
 
 }
